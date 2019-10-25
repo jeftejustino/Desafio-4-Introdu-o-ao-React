304d0425769dda913111abb16c28690add731f36
Jefte <jefte.justino@gmail.com>
2019-10-24 22:21:09 -0300

    melhoria na estilização da pagina

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ function Header(){
   return(
     <header className="topo">
       <div className="flex">
-        <div className="logo">Facebook</div>
+        <div className="logo"><img src="https://i.imgur.com/KDIDiSE.png" alt="logo"/></div>
         <div className="perfil">
-          Meu Perfil s
+          Meu Perfil <i className="material-icons">account_circle</i>
         </div>
       </div>
     </header>
